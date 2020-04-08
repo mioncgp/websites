d987935e1f4baf6bf861d71b7c949ef849fc10ef
Mike <mioncgp@gmail.com>
2020-04-08 11:50:35 +0400

    presentaion

--- websiteTwo/js/main.js
+++ websiteTwo/js/main.js
@@ -1,14 +1,4 @@
 
-function initMap() {
-    const loc = { lat: 42.361145, lng: -71.057083};
-    const map = new google.maps.Map(document.querySelector('.map'),
-    {
-        zoom: 14,
-        center: loc
-    })
-    const marker = new google.maps.Marker({ position: loc, map: map});
-}
-
 
 // Sticky menu background
 window.addEventListener('scroll', function() {
@@ -28,7 +18,7 @@ $('#navbar a, .btn').on('click', function(event) {
   
       $('html, body').animate(
         {
-          scrollTop: $(hash).offset().top - 100
+          scrollTop: $(hash).offset().top - 50
         },
         800
       );
